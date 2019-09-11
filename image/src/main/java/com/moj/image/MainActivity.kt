6a35ee0e976334dd624ce5f7fece86021e2958a9
moj <oujianming@tapmega.com>
2019-09-11 14:51:06 +0800

    修改image组件配置

---
+++ image/src/main/java/com/moj/image/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.moj.image
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * @author : moj
+ * @date : 2019/9/11
+ * @description : 开屏app，只有单独作为app运行时有用
+ */
+class MainActivity : AppCompatActivity() {
+}
